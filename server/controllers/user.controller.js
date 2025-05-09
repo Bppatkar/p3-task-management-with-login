ba2e06eb259cdca13abe229e688e76004bc2afa6
Bppatkar <bhanupratappatkar777@gmail.com>
2025-05-09 18:12:46 +0530

    working on basic things

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,52 @@
+const registerUser = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in Registering User: ", error.message);
+  }
+};
+const loginUser = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in Logging In User: ", error.message);
+  }
+};
+const logoutUser = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in logging Out User: ", error.message);
+  }
+};
+const getAllUsers = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in getting All Users: ", error.message);
+  }
+};
+const getUserById = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in getting Single User: ", error.message);
+  }
+};
+const updateUser = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in Updating User: ", error.message);
+  }
+};
+const deleteUser = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in Deleting User: ", error.message);
+  }
+};
+
+module.exports = {
+  registerUser,
+  loginUser,
+  logoutUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
---
+++ server/middleware/auth.js
@@ -0,0 +1,8 @@
+const verifyToken = async () => {
+  try {
+  } catch (error) {
+    console.log("Error in Verifying Token: ", error.message);
+  }
+};
+
+export default verifyToken;
