44324b713ae5073f7d375c3499c4aa484d5f161b
Bppatkar <bhanupratappatkar777@gmail.com>
2025-05-16 10:24:34 +0530

    change in PORT

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "https://p3-task-management-with-login.vercel.app",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow credentials (cookies, authorization headers, etc.)
